358c45ec68341ba7acb01a817fbf801b2d1583de
benmuratseker <benmuratseker@hotmail.com>
2025-01-22 23:26:53 +0100

    products service added and implemented in catalog

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,17 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from './product.model';
-import { productsArray } from './products-data';
+//import { productsArray } from './products-data';
+import { ProductsService } from './products.service';
 
 @Component({
   selector: 'bot-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.css'],
 })
-export class CatalogComponent {
-  products: Product[] = productsArray;
+export class CatalogComponent implements OnInit {
+  
+  // products: Product[] = productsArray;
+  products: Product[] = [];
   private cart: Product[] = [];
 
-  constructor() { }
+  //adding productService without dependency injection
+  // constructor() { 
+  //   this.products =  new ProductsService().getProducts();
+  // }
+
+  //this one is another way nut not the best
+  // constructor(private productService : ProductsService) {
+  //   this.products = this.productService.getProducts();
+  // }
+
+  constructor(private productsService : ProductsService){}
+  //best way to inject dependencies is onInit
+  ngOnInit() {
+    this.products = this.productsService.getProducts();
+  }
 
   addToCart(product: Product) {
     this.cart.push(product);
